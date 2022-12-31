58e30cae574c382d4b090b3065eb0d21c6879060
Devabubakar <abubakarali3w@gmail.com>
2023-01-01 00:02:05 +0300

    cleaning: typos

--- src/components/Scholars.jsx
+++ src/components/Scholars.jsx
@@ -4,56 +4,55 @@ import team1 from '../assets/images/team-1.png';
 import team2 from '../assets/images/team-2.png';
 import team3 from '../assets/images/team-3.png';
 
-
 const Scholars = () => {
   return (
-    <section class='team' id='team'>
-      <h1 class='heading'>
+    <section className='team' id='team'>
+      <h1 className='heading'>
         our <span>Scholars</span>
       </h1>
 
-      <div class='box-container'>
-        <div class='box'>
-          <div class='image'>
+      <div className='box-container'>
+        <div className='box'>
+          <div className='image'>
             <img src={team1} alt='' />
           </div>
-          <div class='content'>
+          <div className='content'>
             <h3>Sheikh Ali</h3>
             <p>Dawa'ah Chairman</p>
-            <div class='share'>
-              <i class='fab fa-facebook-f'></i>
-              <i class='fab fa-twitter'></i>
-              <i class='fab fa-instagram'></i>
+            <div className='share'>
+              <i className='fab fa-facebook-f'></i>
+              <i className='fab fa-twitter'></i>
+              <i className='fab fa-instagram'></i>
             </div>
           </div>
         </div>
 
-        <div class='box'>
-          <div class='image'>
+        <div className='box'>
+          <div className='image'>
             <img src={team2} alt='' />
           </div>
-          <div class='content'>
+          <div className='content'>
             <h3>Sheikh Ibrahim</h3>
             <p>Dawa'ah Coordinator</p>
-            <div class='share'>
-              <i class='fab fa-facebook-f'></i>
-              <i class='fab fa-twitter'></i>
-              <i class='fab fa-instagram'></i>
+            <div className='share'>
+              <i className='fab fa-facebook-f'></i>
+              <i className='fab fa-twitter'></i>
+              <i className='fab fa-instagram'></i>
             </div>
           </div>
         </div>
 
-        <div class='box'>
-          <div class='image'>
+        <div className='box'>
+          <div className='image'>
             <img src={team3} alt='' />
           </div>
-          <div class='content'>
+          <div className='content'>
             <h3>Sh Hamisi</h3>
             <p>Chairman</p>
-            <div class='share'>
-              <i class='fab fa-facebook-f'></i>
-              <i class='fab fa-twitter'></i>
-              <i class='fab fa-instagram'></i>
+            <div className='share'>
+              <i className='fab fa-facebook-f'></i>
+              <i className='fab fa-twitter'></i>
+              <i className='fab fa-instagram'></i>
             </div>
           </div>
         </div>
