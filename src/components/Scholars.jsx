32345bf72076aef8bc4b16b5d68af639436642e3
Devabubakar <abubakarali3w@gmail.com>
2023-01-07 20:44:12 +0300

    updated leader's name

--- src/components/Scholars.jsx
+++ src/components/Scholars.jsx
@@ -6,19 +6,20 @@ import team1 from '../assets/images/team-1.png';
 import team2 from '../assets/images/team-2.png';
 import team3 from '../assets/images/team-3.png';
 import MultiActionAreaCard from './Card';
+import Typography from '@mui/material/Typography';
 
 import Grid from '@mui/material/Grid';
 
 const Scholars = () => {
   return (
-    <Box sx={{ margin: { xs: '2rem' , md:'6rem'} }}>
-      <h1 class='heading'>
+    <Box sx={{ margin: { xs: '2rem', md: '6rem' } }}>
+      <Typography variant='h3' className='heading'>
         <span>Our</span> Leadership <span>Team</span>
-      </h1>
+      </Typography>
       <Grid container spacing={1}>
         <Grid item xs={12} sm={6} md={4}>
           <MultiActionAreaCard
-            name='Hamisi Ali'
+            name='Ismail Kodi'
             bio='Chairman'
             img={team1}
             props={{ display: 'blog' }}
@@ -27,16 +28,16 @@ const Scholars = () => {
 
         <Grid item xs={12} sm={6} md={4}>
           <MultiActionAreaCard
-            name='Sala Maalim'
-            bio='Tresurer'
+            name='Abdarahman'
+            bio='Assistant Chairman'
             img={team2}
             props={{ display: 'blog' }}
           />
         </Grid>
         <Grid item xs={12} sm={6} md={4}>
           <MultiActionAreaCard
-            name='Abubakar Ali'
-            bio='Assistant Chairman'
+            name='Juma Hamisi'
+            bio='Treasurer'
             img={team3}
             props={{ display: 'blog' }}
           />
