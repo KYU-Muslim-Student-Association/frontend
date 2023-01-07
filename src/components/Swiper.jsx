828eb2373b330c25aa4f712b11d9a1a2a80a6aa8
Devabubakar <abubakarali3w@gmail.com>
2023-01-07 20:43:29 +0300

    removed the carousel functionality

--- src/components/Swiper.jsx
+++ src/components/Swiper.jsx
@@ -1,100 +1,42 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+import Slider from '../assets/one.jpg'
 
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Typography, Box, Button } from '@mui/material';
-import slider1 from '../assets/three.jpg';
-import slider2 from '../assets/one.jpg';
+const styles = {
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '90vh',
+    backgroundColor: 'primary',
+    color: 'primary.contrastText',
+  },
+  image: {
+    objectFit: 'cover',
+    width: '100%',
+    height: '100%',
+  },
+  text: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: `translate(-50%,-50%)`,
+    textAlign: 'center',
+    color:'white'
+  },
+};
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-
-
-
-// import required modules
-import { Navigation } from 'swiper';
-
-export default function App() {
+export default function Hero() {
   return (
-    <>
-      <Swiper
-        navigation={true}
-        modules={[Navigation]}
-        className='mySwiper'
-        style={{ height: '100vh' }}
-      >
-        <SwiperSlide>
-          <img
-            src={slider1}
-            alt='slider2'
-            className='swiper-slide slide'
-            style={{ objectFit: 'cover' }}
-          />
-          <Box
-            sx ={{
-              width: {
-                xs: '90%', // theme.breakpoints.up('xs')
-                sm: '80%', // theme.breakpoints.up('sm')
-                md: '60%', // theme.breakpoints.up('md')
-                // theme.breakpoints.up('xl')
-              },
-            }}
-            style={{
-              position: 'absolute',
-              top: '45%',
-              left: '50%',
-              transform: `translate(-50%,-50%)`,
-              marginTop: 0,
-              textAlign:'center',
-
-            }}
-          >
-            <Typography
-              variant='h3'
-              color='white'
-              sx={{
-                marginBottom:'20px',
-                textAlign: 'center',
-                typography: {md:'h2',sm: 'h4', xs:'h4'},
-                fontWeight:'bolder'
-              }}
-            >
-              WELCOME TO KYUMSA COMMUNITY
-            </Typography>
-            <Button variant='contained' sx={{ bgcolor:'#c98d83'}}>Get Started</Button>
-          </Box>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <img src={slider2} alt='slider2' className='swiper-slide slide' />
-          <Box
-            style={{
-              position: 'absolute',
-              top: '45%',
-              left: '50%',
-              transform: `translate(-50%,-50%)`,
-              marginTop: 0,
-              textAlign:'center',
-              width:{xs:100}
-              
-
-            }}
-          >
-            <Typography
-              variant='h3'
-              color='white'
-              sx={{
-                marginBottom:'20px',
-                textAlign: 'center',
-                typography: {md:'h2',sm: 'h4', xs:'h4'}
-              }}
-            >
-              WELCOME TO KYUMSA COMMUNITY
-            </Typography>
-            <Button variant='contained' sx={{bgcolor:'#c98d83'}}>Get Started</Button>
-          </Box>
-        </SwiperSlide>
-      </Swiper>
-    </>
+    <div style={styles.root}>
+      <img
+        src={Slider}
+        style={styles.image}
+        alt='Hero'
+      />
+      <Typography variant='h2' style={styles.text}>
+        KyUMSA Community
+      </Typography>
+    </div>
   );
 }
