4127e63b0e82d612387ea55695d7097c4532dd0f
Devabubakar <abubakarali3w@gmail.com>
2023-01-11 15:37:14 +0300

    [feat] added prayer schedule and quran verse

---
+++ src/components/PrayerTime.jsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import { Grid } from '@mui/material';
+import Random from './Random';
+
+function createData(prayer, time) {
+  return { prayer, time };
+}
+
+const rows = [
+  createData('Fajr', '05:19 AM'),
+  createData('Dhuhr', '12:45 PM'),
+  createData('Asr', '04:15 PM'),
+  createData('Maghrib', '18:45 PM'),
+  createData('Isha', '20:15 PM'),
+];
+
+export default function PrayerTime() {
+  return (
+    <Container
+      maxWidth='xl'
+      sx={{ marginTop: '20vh', marginBottom: '20vh', textAlign: 'center' }}
+    >
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6}>
+          <Typography
+            variant='h3'
+            className='heading'
+            sx={{ fontWeight: '900' }}
+          >
+            <span>Prayer</span> Times
+          </Typography>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 50 }} aria-label='simple table'>
+              <TableHead>
+                <TableRow>
+                  <TableCell align='center'>Prayer</TableCell>
+                  <TableCell align='center'>Time</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow
+                    key={row.name}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell align='center'>{row.prayer}</TableCell>
+                    <TableCell align='center'>{row.time}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+        <Grid item xs={12} md={6} display='flex' alignItems='center' justifyContent='center'>
+          <Random />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
---
+++ src/components/Random.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Container } from '@mui/system';
+
+import RefreshIcon from '@mui/icons-material/Refresh';
+import { Typography } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+
+
+const Random = () => {
+  const [verse, setVerse] = React.useState({
+    ayat: 'O ye who believe! Be mindful of your duty to Allah, and seek the way of approach unto Him, and strive in His way in order that ye may succeed. ',
+    surah: 'Surah Al-Imran',
+    number: 'Verse 110',
+  });
+
+  const randomAya = Math.random() * (6236 - 1) + 1;
+
+  const getVerse = async () => {
+    try {
+      await fetch(
+        `https://api.alquran.cloud/v1/ayah/${randomAya}/editions/quran-uthmani,en.asad`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setVerse({
+            ayat: data.data[1].text,
+            surah: data.data[1].surah.englishName,
+            number: data.data[1].surah.number,
+          });
+        });
+
+      console.log(verse);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <Container>
+      <IconButton onClick={() => getVerse()}>
+        <RefreshIcon fontSize='large' cursor='pointer' />
+      </IconButton>
+
+      <Typography variant='body1' textAlign='center'>
+        Random Quran Verse
+      </Typography>
+      <Typography variant='h5' m={5} color='#c98d83'>
+        "{verse?.ayat}"
+      </Typography>
+      <Typography variant='body1' textAlign='center'>
+        Surah {verse?.surah} - {verse?.number}
+      </Typography>
+    </Container>
+  );
+};
+
+export default Random;
